f948e4ab2866d5c3326234548defcaac34291fee
vaklarma <biro.andrasgabor.regist@gmail.com>
2018-05-23 22:01:06 +0200

    * pipe demo * listdemo házi kezdés * előzőek kommentezve

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,26 @@ import {AppComponent} from './app.component';
 import {EventComponent} from './event/event.component';
 import { AlmaComponent } from './alma/alma.component';
 import {FormsModule} from '@angular/forms';
+import { ListDemoComponent } from './list-demo/list-demo.component';
+import { ProbaComponensComponent } from './proba-componens/proba-componens.component';
+import { ElvalasztvairjaComponent } from './elvalasztvairja/elvalasztvairja.component';
+import { ElvalasztvaIrjaComponent } from './elvalasztva-irja/elvalasztva-irja.component';
+import { ListdemoComponent } from './listdemo/listdemo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AlmaComponent,
     EventComponent,
+    ListDemoComponent,
+    ProbaComponensComponent,
+    ElvalasztvairjaComponent,
+    ElvalasztvaIrjaComponent,
+    ListdemoComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
